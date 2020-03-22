35f7a6f7368b6339f63173326dfd0280c7d29fb5
liuying <liuying@baibu.la>
2020-03-22 10:59:29 +0800

    增加Windows自动服务工具

---
+++ winservice.bat
@@ -0,0 +1,6 @@
+D:\source_reference\supplies-manage-web\WinSW.NET4.exe install
+net start supplies-manage-web
+::net stop yklisschedule
+::D:\source_reference\SpringBoot-Schedule\WinSW.NET4.exe uninstall
+
+pause
